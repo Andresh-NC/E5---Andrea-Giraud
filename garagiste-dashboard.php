d11d3f267b94ad39b774fdd04c4b566351bba198
Andresh-NC <andrea.giraud1101@gmail.com>
2024-11-14 07:36:50 +1100

    Add files via upload

--- garagiste-dashboard.php
+++ garagiste-dashboard.php
@@ -13,18 +13,14 @@ session_start();
 </head>
 
 <header>
-    <?php
-        echo "<p class ='message'> Bonjour " . $_SESSION['email'] . "</p>";
-    ?>
-
-    <h2>Espace Garagiste</h2>
+    <h2>Bienvenue dans l'espace garagiste</h2>
 </header>
 
 <body>
 <div class="button-section">
 <button onclick="window.location.href='ajouter_vehicule.html'">Ajouter un Véhicule</button>
 <button onclick="window.location.href='supprimer_vehicule.html'">Supprimer un véhicule</button>
-<button onclick="window.location.href='modifier_vehicule.html'">Modifier un véhicule</button>
+<button onclick="window.location.href='modifier_vehicule.php'">Modifier un véhicule</button>
 <button onclick="window.location.href='rechercher_vehicule.html'">Rechercher un véhicule</button>
 <button onclick="window.location.href='historique_rdv.html'">Consulter l'historique de location</button>
 
